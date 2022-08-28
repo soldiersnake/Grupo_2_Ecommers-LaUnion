9366bb39a4f4d88df0df858cf773d98c0fff4f85
Alejandro Gutierrez <alejoguti77@gmail.com>
2022-08-27 23:06:40 -0500

    agregadas las vistas de inicio de sesion y registro con sus respectivas imagenes y estilos al proyecto

--- Servidor-LAUNION/app.js
+++ Servidor-LAUNION/app.js
@@ -12,3 +12,9 @@ app.listen(3001, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
