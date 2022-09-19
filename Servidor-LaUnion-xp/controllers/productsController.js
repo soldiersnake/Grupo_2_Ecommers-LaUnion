f94a860e114bd75f18889462c764776bc6267649
soldiersnake <mariano.maciasgandulfo@gmail.com>
2022-09-19 00:52:31 -0300

    modificacion de formulario

--- Servidor-LaUnion-xp/controllers/productsController.js
+++ Servidor-LaUnion-xp/controllers/productsController.js
@@ -5,13 +5,13 @@ const productsController = {
         let productFound = products.find(producto => {
             return producto.id == req.params.id
         });
-        res.render('./products/detalle-producto', {product:productFound});
+        res.render('./products/detalle-producto', {product : productFound});
     },
     carrito: function(req, res, next) {
         let productToBuy = products.find(producto => {
             return producto.id == req.params.id
         });
-        res.render('./products/carrito', {product:productToBuy});
+        res.render('./products/carrito', {product : productToBuy});
     },
     ABMproducts: function(req, res) {
         res.render("./products/ABMproducts");
