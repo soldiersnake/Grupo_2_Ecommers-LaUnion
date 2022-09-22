864ee26dc9265a97f571854d5df1611699df6497
soldiersnake <mariano.maciasgandulfo@gmail.com>
2022-09-22 17:40:18 -0300

    sprint 3 finalizado

--- Servidor-LaUnion-xp/controllers/productsController.js
+++ Servidor-LaUnion-xp/controllers/productsController.js
@@ -1,4 +1,5 @@
 let products = require('../js/products');
+let categories = require('../js/categories');
 
 const productsController = {
     detalle: function(req, res, next) {
@@ -7,6 +8,17 @@ const productsController = {
         });
         res.render('./products/detalle-producto', {product : productFound});
     },
+    
+    biblioteca: function(req, res){
+        res.render('./products/biblioteca', {products, categories});
+    },
+    juegos: function(req, res){
+        res.render('./products/juegos', {products, categories});
+    },
+    figuras: function(req, res){
+        res.render('./products/figuras', {products, categories});
+    },
+
     carrito: function(req, res, next) {
         let productToBuy = products.find(producto => {
             return producto.id == req.params.id;
--- Servidor-LaUnion-xp/routes/products.js
+++ Servidor-LaUnion-xp/routes/products.js
@@ -11,6 +11,10 @@ router.get('/create', productsController.create);
 router.post('/create', productsController.create);
 
 router.get('/edit/:id', productsController.edit);
-router.put('/edit/:id', productsController.update);
+// router.put('/edit/:id', productsController.update);
+
+router.get('/biblioteca', productsController.biblioteca);
+router.get('/juegos', productsController.juegos);
+router.get('/figuras', productsController.figuras);
 
 module.exports = router;
