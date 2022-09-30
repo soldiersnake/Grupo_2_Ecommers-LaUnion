5dc5b11e447d3bc8b89bc39117ec59e8537d93d5
soldiersnake <mariano.maciasgandulfo@gmail.com>
2022-09-30 20:09:06 -0300

    registro de usuario con multer completo

--- Servidor-LaUnion-xp/controllers/usersController.js
+++ Servidor-LaUnion-xp/controllers/usersController.js
@@ -1,12 +1,46 @@
+const fs = require('fs');
+const path = require('path');
 
+//funcion para enlistar todos los users
+function findAllUsers(){
+  const jsonDataUsers = fs.readFileSync(path.join(__dirname, '../data/users.json'));
+  const users = JSON.parse(jsonDataUsers);
+  return users;
+}
+
+//funcion para añadir los datos a nuestro usersController
+function writeFile(users){
+  // ,null y ,4 sirven para formatear el json
+  const dataString = JSON.stringify(users, null, 4);
+  //sobreescribimos todo el archivo con la nueva informacion:
+  fs.writeFileSync(path.join(__dirname, '../data/users.json'), dataString);
+}
 
 const usersController = {
     login:  function(req, res, next) {
         res.render('./users/login');
       },
-
     register: function(req, res, next) {
         res.render('./users/register');
+      },
+      RegisterUser: (req, res) => {
+        //trae los usuarios
+        const users = findAllUsers();
+        // utilizamos los campos del formulario para crear el nuevo objeto/usuario
+        const newUsers = {
+          user: req.body.user,
+          name: req.body.name,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          password: req.body.password1,
+          confirmPassword: req.body.password2,
+          imgPerfil: req.file.filename,
+        };
+
+        users.push(newUsers);
+        writeFile(users);
+
+        res.redirect('/');
       },
 }
 
--- Servidor-LaUnion-xp/routes/users.js
+++ Servidor-LaUnion-xp/routes/users.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const usersController = require('../controllers/usersController');
+const path = require('path');
+const multer = require('multer');
+
+//configuramos multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../public/imgUsers'))
+    },
+    filename: (req, file, cb) => {
+        const newFileName = file.fieldname + Date.now() + path.extname(file.originalname);
+        cb(null, newFileName);
+    }
+});
+const upload = multer({storage:storage});
 
 /* GET users listing. */
 router.get('/login', usersController.login);
 
 router.get('/register', usersController.register);
+router.post('/register', upload.single("avatar"), usersController.RegisterUser);
 
 module.exports = router;
